d06f8fc1daa6e7e5459a6db4b7d4fb31445ec6ce
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-10-29 23:58:31 -0300

    init css
    gui modifications

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
-
 const electron = require('electron')
 var path = require('path')
 const { app, BrowserWindow } = require('electron')
@@ -17,28 +16,33 @@ function createWindow () {
   })
   win.loadFile('pages/index.html')
 }
-
-function bdsmaneger() {
-  var spawn = require('child_process').spawn;
-  var child = spawn('command -v bds-maneger', {
-    shell: true
-  });
-  child.on('exit', function (code) {
-    console.log (`code ${code}`)
-    if (code == 0){
-      app.whenReady().then(createWindow);
-    } else {
-      alert(`Exited it BDS-Manager is not installed, code ${code}`);
-      app.quit();
+function createWindowerro () {
+  // Cria uma janela de navegação de erro.
+  const win = new BrowserWindow({
+    titleBarStyle: 'hidden',
+    width: 300,
+    height: 70,
+    icon: path.join(__dirname, 'pages/assents/mcpe.png'),
+    webPreferences: {
+      sandbox: true
     }
-  });
-};
-bdsmaneger();
+  })
+  win.loadFile('erro.html')
+}
+var spawn = require('child_process').spawn;
+var child = spawn('command -v bds-maneger', {
+  shell: true
+});
+child.on('exit', function (code) {
+  console.log (`code ${code}`)
+  if (code == 0){
+    app.whenReady().then(createWindow);
+  } else {
+    app.whenReady().then(createWindowerro);
+    // app.quit();
+  }
+});
 //  app.whenReady().then(createWindow)
-// Este método será chamado quando Electron terminar de inicializar
-// e também estiver pronto para criar novas janelas do navegador.
-// Algumas APIs podem ser usadas somente depois que este evento ocorre.
-
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
